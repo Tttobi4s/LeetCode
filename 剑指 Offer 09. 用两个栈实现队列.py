6c7ce562a03224fb7821669a6e8deed87abd5e44
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-02-22 13:39:04 +0800

    LeetCode

--- 剑指 Offer 09. 用两个栈实现队列.py
+++ 剑指 Offer 09. 用两个栈实现队列.py
@@ -1,6 +1,14 @@
 class CQueue:
     def __init__(self):
+        self.A, self.B = [], []
 
     def deleteHead(self) -> int:
+        if self.B:
+            return self.B.pop()
+        else:
+            while self.A:
+                self.B.append(self.A.pop())
+        return self.B.pop() if self.B else -1
 
     def appendTail(self, value: int) -> None:
+        self.A.append(value)
