0ae8c147af7faab8e75af9ddd8dfa6587e65b3ec
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-02-27 16:16:45 +0800

    LeetCode

---
+++ 剑指 Offer 11. 旋转数组的最小数字.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def minArray(self, numbers: List[int]) -> int:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if numbers[mid] < numbers[left]:
+                right = mid
+            elif numbers[mid] > numbers[left]:
+                left = mid + 1
+            else:
+                left += 1
+        return numbers[left]
+
+s = Solution()
+s.minArray([10,1,10,10,10])
