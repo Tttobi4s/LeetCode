ab68778076ab92a6809562f41f5b30e258822dad
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-02-28 16:56:34 +0800

    LeetCode

---
+++ 剑指 Offer 32 - II. 从上到下打印二叉树 II.py
@@ -0,0 +1,26 @@
+import collections
+from typing import List
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        queue, res = collections.deque(), []
+        queue.append(root)
+        while queue:
+            tmp = []
+            for i in range(len(queue)):
+                cur = queue.popleft()
+                tmp.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            res.append(tmp)
+        return res
