b197ad5b2c687a8808e19b3551f9cff981df834b
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-03-01 13:15:39 +0800

    LeetCode

--- 剑指 Offer 26. 树的子结构.py
+++ 剑指 Offer 26. 树的子结构.py
@@ -7,13 +7,14 @@ class TreeNode:
 
 class Solution:
     def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+        def recur(A, B):
+            if not B:
+                return True
+            if not A or A.val != B.val:
+                return False
+            return recur(A.left, B.left) and recur(A.right, B.right)
+
         if not B or not A:
             return True
-        return self.recur(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
+        return recur(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
 
-    def recur(self, A, B):
-        if not B:
-            return True
-        if not A or A.val != B.val:
-            return False
-        return self.recur(A.left, B.left) and self.recur(A.right, B.right)
