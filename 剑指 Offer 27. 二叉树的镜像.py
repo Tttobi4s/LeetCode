cc2e1a4eda19bd7b76c3fccd4f5a6cf57164fa98
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-03-01 13:46:33 +0800

    LeetCode

---
+++ 剑指 Offer 27. 二叉树的镜像.py
@@ -0,0 +1,39 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution1:
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        root.right, root.left = self.mirrorTree(root.left), self.mirrorTree(root.right)
+        return root
+
+
+class Solution2:
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+            cur.left, cur.right = cur.right, cur.left
+        return root
+
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        def recur(root):
+            if not root:
+                return
+            root.left, root.right = root.right, root.left
+            recur(root.left)
+            recur(root.right)
+
+        recur(root)
+        return root
