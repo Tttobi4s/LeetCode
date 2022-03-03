886b100809737612563b89380fb49fc82f24dd8a
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-03-03 09:32:47 +0800

    LeetCode

---
+++ 剑指 Offer 42. 连续子数组的最大和.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res, pre = nums[0], nums[0]
+        for i in range(1, len(nums)):
+            if pre < 0:
+                cur = nums[i]
+            else:
+                cur = pre + nums[i]
+            res = max(res, cur)
+            pre = cur
+        return res
