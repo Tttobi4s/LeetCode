31ca1cf1b8d3c045af65503ffdfc5389789d7971
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-03-06 10:18:04 +0800

    LeetCode

--- 剑指 Offer 25. 合并两个排序的链表.py
+++ 剑指 Offer 25. 合并两个排序的链表.py
@@ -9,14 +9,10 @@ class Solution:
         while l1 and l2:
             if l1.val <= l2.val:
                 r.next = l1
-                r = l1
                 l1 = l1.next
             else:
                 r.next = l2
-                r = l2
                 l2 = l2.next
-        if l1:
-            r.next = l1
-        if l2:
-            r.next = l2
+            r = r.next
+        r.next = l1 if l1 else l2
         return head.next
