7038c270d28bd876360125d4c6cc0c8abfda10a5
Tttobi4s <51215902053@stu.ecnu.edu.cn>
2022-03-21 20:59:24 +0800

    LeetCode

---
+++ 剑指 Offer 55 - I. 二叉树的深度.py
@@ -0,0 +1,29 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution1:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left),self.maxDepth(root.right)) + 1;
+
+
+class Solution2:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        queue, res = [root], 0
+        while queue:
+            tmp = []
+            for node in queue:
+                if node.left:
+                    tmp.append(node.left)
+                if node.right:
+                    tmp.append(node.right)
+            queue = tmp
+            res += 1
+        return res
+
